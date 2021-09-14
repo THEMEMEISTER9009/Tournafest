72aee0fd27e5da33de609d95a1fa992d1fd80c9b
sg23x <soumyagupta2307@gmail.com>
2021-09-14 20:27:43 +0530

    made item list tile

---
+++ lib/widgets.dart/movieItem.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:technofest/constants.dart';
+
+class MovieItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Card(
+          elevation: 0,
+          color: colBlue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          margin: EdgeInsets.only(top: 20),
+          child: Stack(
+            children: [
+              Container(
+                height: height * 0.17,
+                width: width * 0.85,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: height / 7,
+                      decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        borderRadius: BorderRadius.circular(
+                          30,
+                        ),
+                        color: Colors.white,
+                      ),
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: height * 0.115,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              SizedBox(),
+                              Text(
+                                "Gardians of the galaxy vol. 2",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Animation | Drama",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    size: height * 0.02,
+                                    color: colOrange,
+                                  ),
+                                  SizedBox(
+                                    width: width * 0.005,
+                                  ),
+                                  Text(
+                                    "7.1",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10,
+                  ),
+                ),
+                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                height: height / 7,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets.dart/tab1.dart
+++ lib/widgets.dart/tab1.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:technofest/widgets.dart/movieItem.dart';
 
 class PopularTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView(
+      children: [
+        MovieItem(),
+        MovieItem(),
+      ],
+    );
   }
 }
